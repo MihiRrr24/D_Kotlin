3e4f1a59692d4ee78522da943f31cc87a37f5d35
Mihir Shrestha <121042506+MihiRrr24@users.noreply.github.com>
2024-08-13 23:15:15 +0530

    Basics

---
+++ Day 1/basics.kt
@@ -0,0 +1,27 @@
+// -> single line comment
+/* -> multi line comment   */
+
+
+//fun main(args:Array<String>)            OR
+fun main(){
+    var x = 50
+    println(x)              // or print(x)
+
+/*    var y = 5
+       y=7              // y changes to 7  coz it is var
+      print(y)
+*/
+
+/*     val y = 5
+       y=7               // cannot be reassigned  coz it is val
+       print(y)
+*/
+
+    // Declaring with type of variable
+    var z:Int = 70
+    println(z)
+
+    // Using keywords as variable
+    var `if` = "Mihir"
+    println(`if`)
+}
