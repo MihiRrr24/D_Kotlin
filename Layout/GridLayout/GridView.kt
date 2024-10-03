cc85375556f34ad4a2ff68a3ae2e15332d45e486
Mihir Shrestha <121042506+MihiRrr24@users.noreply.github.com>
2024-10-03 17:55:27 +0530

    Add files via upload

---
+++ Layout/GridLayout/GridView.kt
@@ -0,0 +1,20 @@
+package com.example.cse224
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class GridView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_grid_view)
+
+        val a = findViewById<ImageButton>(R.id.imageButton3)
+
+
+    }
+}
